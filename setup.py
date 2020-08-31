ee21021a013cd3b2493b9bd6cd00759efa15df5b
Sergey Kolosov <m17.admin@gmail.com>
2020-08-31 10:04:44 +0300

    Added classifiers and maintainer info to setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,21 @@ setup(
     description="Bypass supervisor's nasty callbacks stack and make it quick!",
     author="Lx Yu",
     author_email="i@lxyu.net",
+    maintainer="Sergey Kolosov",
+    maintainer_email="m17.admin@gmail.com",
     py_modules=["supervisor_quick", ],
     package_data={"": ["LICENSE"], },
-    url="http://lxyu.github.io/supervisor-quick/",
+    url="https://github.com/sergeykolosov/supervisor-quick",
     license="MIT",
     long_description=open("README.rst").read(),
     install_requires=[
         "supervisor",
     ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
 )
